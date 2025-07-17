2e0546200f435d03f7ec1494cdbc9ac4c1d4865b
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2025-07-16 22:41:49 -0400

    First commit

---
+++ src/main/java/voidsong/surfacepolishing/SurfacePolishing.java
@@ -0,0 +1,12 @@
+package voidsong.surfacepolishing;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(SurfacePolishing.MODID)
+public class SurfacePolishing {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "surfacepolishing";
+
+    public SurfacePolishing(IEventBus modEventBus) { }
+}
