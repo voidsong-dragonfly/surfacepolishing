c01b3530485a66a1b0d8c25df4755ab25c34e5e1
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2025-07-20 15:21:10 -0400

    Upload changes to mixins

--- src/main/java/voidsong/surfacepolishing/mixin/SurfaceRulesMixin.java
+++ src/main/java/voidsong/surfacepolishing/mixin/SurfaceRulesMixin.java
@@ -1,149 +1,96 @@
 package voidsong.surfacepolishing.mixin;
 
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.PositionalRandomFactory;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.placement.CaveSurface;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.function.Predicate;
 
-@SuppressWarnings("unused")
-@Mixin(SurfaceRules.class)
-public abstract class SurfaceRulesMixin {
-
-    @Mixin(SurfaceRules.BiomeConditionSource.class)
-    public static final class BiomeConditionSource{
-        @Final
-        @Shadow
-        Predicate<ResourceKey<Biome>> biomeNameTest;
-        /**
-         * @author VoidsongDragonfly
-         * @reason Replacing Vanilla's use of {@link SurfaceRules.LazyYCondition LazyYCondition} that causes performance detriments due to unused caching behavior
-         */
-        @Overwrite
-        public SurfaceRules.Condition apply(final SurfaceRules.Context pContext) {
-            class BiomeCondition implements SurfaceRules.Condition {
-                @Override
-                public boolean test() {
-                    return pContext.biome.get().is(biomeNameTest);
-                }
-            }
-
-            return new BiomeCondition();
-        }
-    }
-
-    @Mixin(SurfaceRules.StoneDepthCheck.class)
+public class SurfaceRulesMixin {
+    @Mixin(value = SurfaceRules.StoneDepthCheck.class, priority = 100)
     public record StoneDepthCheck(int offset, boolean addSurfaceDepth, int secondaryDepthRange, CaveSurface surfaceType) {
-        /**
-         * @author VoidsongDragonfly
-         * @reason Replacing Vanilla's use of {@link SurfaceRules.LazyYCondition LazyYCondition} that causes performance detriments due to unused caching behavior
-         */
-        @Overwrite
-        public SurfaceRules.Condition apply(final SurfaceRules.Context pContext) {
-            // Copied Vanilla variables
-            final boolean flag = this.surfaceType == CaveSurface.CEILING;
-
-            class StoneDepthCondition implements SurfaceRules.Condition {
-                @Override
-                public boolean test() {
-                    int i = flag ? pContext.stoneDepthBelow : pContext.stoneDepthAbove;
-                    int j = addSurfaceDepth ? pContext.surfaceDepth : 0;
-                    int k = secondaryDepthRange == 0
-                        ? 0
-                        : (int)Mth.map(pContext.getSurfaceSecondary(), -1.0, 1.0, 0.0, secondaryDepthRange);
-                    return i <= 1 + offset + j + k;
-                }
-            }
-
-            return new StoneDepthCondition();
+        @Inject(method= "apply(Lnet/minecraft/world/level/levelgen/SurfaceRules$Context;)Lnet/minecraft/world/level/levelgen/SurfaceRules$Condition;", at = @At("HEAD"), cancellable = true)
+        public void apply(SurfaceRules.Context pContext, CallbackInfoReturnable<SurfaceRules.Condition> cir) {
+            cir.setReturnValue(new PerformantStoneDepthCheck(pContext, offset, addSurfaceDepth, secondaryDepthRange, this.surfaceType == CaveSurface.CEILING));
         }
     }
 
-    @Mixin(SurfaceRules.VerticalGradientConditionSource.class)
+    record PerformantStoneDepthCheck(SurfaceRules.Context pContext, int offset, boolean addSurfaceDepth, int secondaryDepthRange, boolean surfaceCeiling) implements SurfaceRules.Condition {
+        @Override
+        public boolean test() {
+            int i = surfaceCeiling ? pContext.stoneDepthBelow : pContext.stoneDepthAbove;
+            int j = addSurfaceDepth ? pContext.surfaceDepth : 0;
+            int k = secondaryDepthRange == 0
+                ? 0
+                : (int)Mth.map(pContext.getSurfaceSecondary(), -1.0, 1.0, 0.0, secondaryDepthRange);
+            return i <= 1 + offset + j + k;
+        }
+    }
+
+    @Mixin(value = SurfaceRules.VerticalGradientConditionSource.class, priority = 100)
     public record VerticalGradientConditionSource(ResourceLocation randomName, VerticalAnchor trueAtAndBelow, VerticalAnchor falseAtAndAbove) {
-        /**
-         * @author VoidsongDragonfly
-         * @reason Replacing Vanilla's use of {@link SurfaceRules.LazyYCondition LazyYCondition} that causes performance detriments due to unused caching behavior
-         */
-        @Overwrite
-        public SurfaceRules.Condition apply(final SurfaceRules.Context pContext) {
-            // Copied Vanilla variables
-            final int i = this.trueAtAndBelow().resolveY(pContext.context);
-            final int j = this.falseAtAndAbove().resolveY(pContext.context);
-            final PositionalRandomFactory positionalrandomfactory = pContext.randomState.getOrCreateRandomFactory(this.randomName());
-
-            class VerticalGradientCondition implements SurfaceRules.Condition {
-                @Override
-                public boolean test() {
-                    int k = pContext.blockY;
-                    if (k <= i) {
-                        return true;
-                    } else if (k >= j) {
-                        return false;
-                    } else {
-                        double d0 = Mth.map(k, i, j, 1.0, 0.0);
-                        RandomSource randomsource = positionalrandomfactory.at(pContext.blockX, k, pContext.blockZ);
-                        return (double)randomsource.nextFloat() < d0;
-                    }
-                }
-            }
-
-            return new VerticalGradientCondition();
+        @Inject(method= "apply(Lnet/minecraft/world/level/levelgen/SurfaceRules$Context;)Lnet/minecraft/world/level/levelgen/SurfaceRules$Condition;", at = @At("HEAD"), cancellable = true)
+        public void apply(SurfaceRules.Context pContext, CallbackInfoReturnable<SurfaceRules.Condition> cir) {
+            cir.setReturnValue(new PerformantVerticalGradientCondition(pContext, pContext.randomState.getOrCreateRandomFactory(this.randomName()), trueAtAndBelow.resolveY(pContext.context), falseAtAndAbove.resolveY(pContext.context)));
         }
     }
 
-    @Mixin(SurfaceRules.WaterConditionSource.class)
+    record PerformantVerticalGradientCondition(SurfaceRules.Context pContext, PositionalRandomFactory randomFactory, int trueAtAndBelow, int falseAtAndAbove) implements SurfaceRules.Condition {
+        @Override
+        public boolean test() {
+            int y = pContext.blockY;
+            if (y <= trueAtAndBelow) {
+                return true;
+            } else if (y >= falseAtAndAbove) {
+                return false;
+            } else {
+                double d0 = Mth.map(y, trueAtAndBelow, falseAtAndAbove, 1.0, 0.0);
+                RandomSource randomSource = randomFactory.at(pContext.blockX, y, pContext.blockZ);
+                return (double)randomSource.nextFloat() < d0;
+            }
+        }
+    }
+
+    @Mixin(value = SurfaceRules.WaterConditionSource.class, priority = 100)
     public record WaterConditionSource(int offset, int surfaceDepthMultiplier, boolean addStoneDepth) {
-        /**
-         * @author VoidsongDragonfly
-         * @reason Replacing Vanilla's use of {@link SurfaceRules.LazyYCondition LazyYCondition} that causes performance detriments due to unused caching behavior
-         */
-        @Overwrite
-        public SurfaceRules.Condition apply(final SurfaceRules.Context pContext) {
-            class WaterCondition implements SurfaceRules.Condition {
-                @Override
-                public boolean test() {
-                    return pContext.waterHeight == Integer.MIN_VALUE
-                        || pContext.blockY + (addStoneDepth ? pContext.stoneDepthAbove : 0)
-                        >= pContext.waterHeight
-                        + offset
-                        + pContext.surfaceDepth * surfaceDepthMultiplier;
-                }
-            }
-
-            return new WaterCondition();
+        @Inject(method= "apply(Lnet/minecraft/world/level/levelgen/SurfaceRules$Context;)Lnet/minecraft/world/level/levelgen/SurfaceRules$Condition;", at = @At("HEAD"), cancellable = true)
+        public void apply(SurfaceRules.Context pContext, CallbackInfoReturnable<SurfaceRules.Condition> cir) {
+            cir.setReturnValue(new PerformantWaterConditionSource(pContext, offset, surfaceDepthMultiplier, addStoneDepth));
         }
-
     }
 
-    @Mixin(SurfaceRules.YConditionSource.class)
-    public record YConditionSource(VerticalAnchor anchor, int surfaceDepthMultiplier, boolean addStoneDepth) {
-        /**
-         * @author VoidsongDragonfly
-         * @reason Replacing Vanilla's use of {@link SurfaceRules.LazyYCondition LazyYCondition} that causes performance detriments due to unused caching behavior
-         */
-        @Overwrite
-        public SurfaceRules.Condition apply(final SurfaceRules.Context pContext) {
-            class YCondition implements SurfaceRules.Condition {
-                @Override
-                public boolean test() {
-                    return pContext.blockY + (addStoneDepth ? pContext.stoneDepthAbove : 0)
-                        >= anchor.resolveY(pContext.context)
-                        + pContext.surfaceDepth * surfaceDepthMultiplier;
-                }
-            }
+    record PerformantWaterConditionSource(SurfaceRules.Context pContext, int offset, int surfaceDepthMultiplier, boolean addStoneDepth) implements SurfaceRules.Condition {
+        @Override
+        public boolean test() {
+            return pContext.waterHeight == Integer.MIN_VALUE
+                || pContext.blockY + (addStoneDepth ? pContext.stoneDepthAbove : 0)
+                >= pContext.waterHeight
+                + offset
+                + pContext.surfaceDepth * surfaceDepthMultiplier;
+        }
+    }
 
-            return new YCondition();
+    @Mixin(value = SurfaceRules.YConditionSource.class, priority = 100)
+    public record YConditionSource(VerticalAnchor anchor, int surfaceDepthMultiplier, boolean addStoneDepth) {
+        @Inject(method= "apply(Lnet/minecraft/world/level/levelgen/SurfaceRules$Context;)Lnet/minecraft/world/level/levelgen/SurfaceRules$Condition;", at = @At("HEAD"), cancellable = true)
+        public void apply(SurfaceRules.Context pContext, CallbackInfoReturnable<SurfaceRules.Condition> cir) {
+            cir.setReturnValue(new PerformantYCondition(pContext, anchor, surfaceDepthMultiplier, addStoneDepth));
+        }
+    }
+
+    record PerformantYCondition(SurfaceRules.Context pContext, VerticalAnchor anchor, int surfaceDepthMultiplier, boolean addStoneDepth) implements SurfaceRules.Condition {
+        @Override
+        public boolean test() {
+            return pContext.blockY + (addStoneDepth ? pContext.stoneDepthAbove : 0)
+                >= anchor.resolveY(pContext.context)
+                + pContext.surfaceDepth * surfaceDepthMultiplier;
         }
     }
 }
